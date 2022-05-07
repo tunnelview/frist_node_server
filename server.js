beef9948b4ee59928f24daef053b1957ba730074
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-07 14:19:59 +1000

    Add structure, style file, file path to front end

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+// const express = require("express");
+const app = express();
+
+import fs from "fs";
+// const fs = require("fs");
+// const path = require("path");
+
+import path from "path";
+const __dirname = path.resolve();
+
+const dt = "my book";
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/form.html");
+  const html = `<div>
+    <h1>This is my first SSR app</h1>
+    <p> I have just created server side rendering app with node and expressJS</p>
+  </div>;
+  `;
+});
+
+app.listen(8000, (error) => {
+  if (error) {
+    console.log("Error");
+  }
+
+  console.log("Server is running at http://localhost:8000");
+});
