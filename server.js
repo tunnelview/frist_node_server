fc06368ea6721c2ab1dd4bc8eb70f72f21e43ec6
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-08 10:52:24 +1000

    Add code to read from file

--- server.js
+++ server.js
@@ -14,15 +14,54 @@ app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/form.html");
 });
 
+const fn = __dirname + "/userList.text";
+
 app.post("/register", (req, res) => {
   console.log(req.body);
   // store in db
+
+  const str1 = Object.values(req.body).join("|") + "\n";
+  console.log(str1);
+
+  // use fs to create a file
+
+  fs.writeFile(fn, str1, (error) => {
+    if (error) {
+      console.log(error);
+    }
+
+    console.log("data is added");
+  });
 
   res.send("Registered Successfully");
 });
 
 app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  fs.readFile(fn, (error, data) => {
+    error && console.log(error);
+
+    const userList = data.toString().split("\n");
+
+    const found = userList.some((item) => {
+      // userList.map((item) => {
+      const userArg = item.split("|");
+      const userEmail = userArg[1];
+      const userPass = userArg[2];
+
+      if (email === userEmail && password === userPass) {
+        return true;
+      }
+    });
+
+    console.log(found);
+    found ? res.send("Login Successfully") : res.send("Login Failed");
+  });
 });
 
 app.get("/", (req, res) => {
